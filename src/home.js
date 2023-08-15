4d8564e73329f2562d661efdb9d0ce6090f8935d
Grecia Fragoso <naomistereo96@gmail.com>
2023-08-15 12:33:19 -0500

    div id root at index.html; title, button text and descriptive text for app at home.html

--- src/home.js
+++ src/home.js
@@ -3,16 +3,20 @@
 function home(navigateTo) {
     const section = document.createElement('section');
     const title = document.createElement('h2');
+    const homeText = document.createElement('h3');
+    const homeText2 = document.createElement('p');
     const button = document.createElement('button');
   
-    button.textContent = 'login';
+    button.textContent = 'Saltar';
     button.addEventListener('click', () => {
       navigateTo('/login');
     });
   
-    title.textContent = 'Welcome to my project';
+    title.textContent = '¡Bienvenida/o a VitalHub!';
+    homeText.textContent = 'Porque creemos en el balance...';
+    homeText2.textContent = 'Comparte tus recetas y rutinas de ejercicio en un entorno saludable.';
   
-    section.append(title, button);
+    section.append(button, title, homeText, homeText2);
     return section;
   }
   

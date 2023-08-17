496f74cd055d8d6b0ff107a97041c77782775e3c
Grecia Fragoso <naomistereo96@gmail.com>
2023-08-17 12:52:05 -0500

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,36 +1,38 @@
-import { initializeApp } from 'firebase/app'; //Función para inicializar la app
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+// eslint-disable-next-line import/no-unresolved
+import { initializeApp } from 'firebase/app'; // Función para inicializar la app
+// eslint-disable-next-line import/no-unresolved
+import { getAuth } from 'firebase/auth';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
+// -------------Firebase----------------------------------------//
+// Objeto de configuración Firebase que pasa la función de inicialización
+// Devuelve una instancia de aplicación Firebase
+// SDK
 
-//-------------Firebase----------------------------------------//
-//Objeto de configuración Firebase que pasa la función de inicialización
-//Devuelve una instancia de aplicación Firebase
-//SDK
+const firebaseConfig = {
+  apiKey: 'AIzaSyCabIXmNKkbhps1_MJUAYHUcVXKuTs9Tn4',
+  authDomain: 'vitalhub-4457d.firebaseapp.com',
+  projectId: 'vitalhub-4457d',
+  storageBucket: 'vitalhub-4457d.appspot.com',
+  messagingSenderId: '801175473249',
+  appId: '1:801175473249:web:d23a4220f3a29d309e6e20',
+};
 
-const app = initializeApp({
-    apiKey: "AIzaSyCabIXmNKkbhps1_MJUAYHUcVXKuTs9Tn4",
-    authDomain: "vitalhub-4457d.firebaseapp.com",
-    projectId: "vitalhub-4457d",
-    storageBucket: "vitalhub-4457d.appspot.com",
-    messagingSenderId: "801175473249",
-    appId: "1:801175473249:web:d23a4220f3a29d309e6e20"
-  });  
-  
-  //Funciones que toman el servicio de FB como primer parámetro.
-  const auth = getAuth(app);
-  //const db = getFirestore(firebaseApp);
-  //const todosCol = collection(db,'todos');
-  //const snapshot = await getDocs(todosCol);
-  
-  //Detecta el estado de autentificación
-  onAuthStateChanged(auth, user => {
-  if(user != null){
+// Funciones que toman el servicio de FB como primer parámetro.
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
-    console.log('logged in');
-  }else{
-  console.log('No user');
-  }
-  });
-  
-  export default initializeApp;
+// const db = getFirestore(firebaseApp);
+// const todosCol = collection(db,'todos');
+// const snapshot = await getDocs(todosCol);
+
+// Detecta el estado de autentificación
+// onAuthStateChanged(auth, user => {
+//   if (user != null) {
+//     console.log('logged in');
+//   } else {
+//     console.log('No user');
+//   }
+// });
+
+// export default initializeApp;
--- src/views/login.js
+++ src/views/login.js
@@ -1,4 +1,6 @@
 // file login finished
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from 'firebase/auth';
 import imgLogin from '../assets/img/hombre-entrenando-verde.png';
 
 function login(navigateTo) {
@@ -12,11 +14,28 @@ function login(navigateTo) {
   const buttonLogin = document.createElement('button');
 
   inputEmail.placeholder = 'Escribe tu email';
+  inputEmail.type = 'email'; // Input para insertar email
   inputPass.placeholder = 'Contraseña';
+  inputPass.type = 'password'; // Contraseña no visible
+  inputPass.pattern = '.{6,}';
 
   title.textContent = 'Inicia sesión';
   imageLogin.src = imgLogin;
   buttonLogin.textContent = 'Iniciar sesión';
+  buttonLogin.addEventListener('click', async (e) => {
+    e.preventDefault(); // Evita que se recargue la página web
+    // eslint-disable-next-line max-len
+    const email = inputEmail.value; // Toma los valores cuando se da click al botón de iniciar sesión
+    const userPassword = inputPass.value;
+    console.log(email, userPassword);
+
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, userPassword);
+      console.log(userCredentials);
+    } catch (error) {
+      console.log(error)
+    }
+  });
 
   buttonReturn.textContent = 'Regresar a inicio';
   buttonReturn.addEventListener('click', () => {

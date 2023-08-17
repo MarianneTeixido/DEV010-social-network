8f833896179604b3446adbbc4746bee1f26ec108
MarFueGit <maricelafuentes100@gmail.com>
2023-08-17 11:00:04 -0600

    fix: se arreglo comillas

--- src/views/home.js
+++ src/views/home.js
@@ -18,31 +18,30 @@ function home(navigateTo) {
 
   //   </div>
   //   </section>
-  const section = document.createElement("section");
-  section.classList.add("section-home");
-  const divTitle = document.createElement("div");
-  divTitle.classList.add("titles");
-  const button = document.createElement("button");
-  button.setAttribute("id", "button");
-  button.innerHTML = "SKIP";
-  button.addEventListener("click", () => {
-    navigateTo("/login");
+  const section = document.createElement('section');
+  section.classList.add('section-home');
+  const divTitle = document.createElement('div');
+  divTitle.classList.add('titles');
+  const button = document.createElement('button');
+  button.setAttribute('id', 'button');
+  button.innerHTML = 'SKIP';
+  button.addEventListener('click', () => {
+    navigateTo('/login');
   });
 
-  const welcome = document.createElement("h2");
-  welcome.classList.add("welcome");
-  welcome.innerHTML = "¡Welcome to VitalHub!";
-  const divImg = document.createElement("div");
-  const img = document.createElement("img");
-  img.classList.add("img");
-  img.src = "../assets/img/entrenando-azul.png";
-  img.alt = "imagen entrenando-azul";
+  const welcome = document.createElement('h2');
+  welcome.classList.add('welcome');
+  welcome.innerHTML = '¡Welcome to VitalHub!';
+  const divImg = document.createElement('div');
+  const img = document.createElement('img');
+  img.classList.add('img');
+  img.src = '../assets/img/entrenando-azul.png';
+  img.alt = 'imagen entrenando-azul';
 
-  const homeText = document.createElement("h3");
-  homeText.innerHTML = "Because we belive in balance ...";
-  const homeText2 = document.createElement("p");
-  homeText2.innerHTML =
-    "Share your recipes and exercise routines in a healthy environment.";
+  const homeText = document.createElement('h3');
+  homeText.innerHTML = 'Because we belive in balance ...';
+  const homeText2 = document.createElement('p');
+  homeText2.innerHTML = 'Share your recipes and exercise routines in a healthy environment.';
   divImg.appendChild(img);
   divTitle.append(button, welcome, divImg, homeText, homeText2);
   section.appendChild(divTitle);

07b5eff49f8a0f0ea993b82e0bd79d07facb242a
Grecia Fragoso <naomistereo96@gmail.com>
2023-08-28 12:19:59 -0500

    footer for navigation bar added at feed, recipes, workout and profile views

--- src/views/profile.js
+++ src/views/profile.js
@@ -2,10 +2,12 @@ import navigationBar from './navigationBar';
 
 function profile(navigateTo) {
   const body = document.createElement('body');
+  const footer = document.createElement('footer');
   const profileTitle = document.createElement('h1');
   profileTitle.textContent = 'Profile';
   console.log('Profile');
-  body.append(profileTitle, navigationBar(navigateTo));
+  footer.append(navigationBar(navigateTo));
+  body.append(profileTitle, footer);
 }
 
 export default profile;
--- src/views/workout.js
+++ src/views/workout.js
@@ -2,8 +2,12 @@ import navigationBar from './navigationBar';
 
 function workout(navigateTo) {
   const body = document.createElement('body');
+  const workoutTitle = document.createElement('h1');
+  workoutTitle.textContent = 'Workout';
+  const footer = document.createElement('footer');
   console.log('Workout');
-  body.append(navigationBar(navigateTo));
+  footer.append(navigationBar(navigateTo));
+  body.append(workoutTitle, footer);
 }
 
 export default workout;
